ba0a5f4ebe92680096391fc22a561339ddaf21bb
stevenabi6912-prog <stevenabi6912@gmail.com>
2026-02-24 17:18:16 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
-export const firebaseConfig = {
+// firebase-config.js
+
+const cfg = {
   apiKey: "AIzaSyAWup4eYIUD6aYnMrYwFUmgnUZXsVyKWys",
   authDomain: "faithkidstrivia.firebaseapp.com",
   projectId: "faithkidstrivia",
@@ -7,3 +9,7 @@ export const firebaseConfig = {
   appId: "1:295258301478:web:cb507f3a9fa2e87a0ff2e7",
   measurementId: "G-FND4TP0S2P"
 };
+
+// Export BOTH names so whichever one the app imports will exist.
+export const firebaseConfig = cfg;
+export const firebaseConfig = cfg;
