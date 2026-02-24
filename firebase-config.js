867f50bf49222afaa385525a58f589e36533eb78
stevenabi6912-prog <stevenabi6912@gmail.com>
2026-02-24 14:29:06 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+const firebaseConfig = {
+  apiKey: "AIzaSyAWup4eYIUD6aYnMrYwFUmgnUZXsVyKWys",
+  authDomain: "faithkidstrivia.firebaseapp.com",
+  projectId: "faithkidstrivia",
+  storageBucket: "faithkidstrivia.firebasestorage.app",
+  messagingSenderId: "295258301478",
+  appId: "1:295258301478:web:cb507f3a9fa2e87a0ff2e7",
+  measurementId: "G-FND4TP0S2P"
 };
